fb2c78d9335815efdb089864849372e4082b256b
kiranvswami <85357992+kiranvswami@users.noreply.github.com>
2021-06-06 13:57:32 +0530

    Update Find_element.py

--- Find_element.py
+++ Find_element.py
@@ -9,7 +9,7 @@ print(driver.title) #find the title of the webpage
 
 driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[2]/div[1]/div/div/a/button/span[1]/span").click() #locate the Fiction button and click
 
-element=driver.find_element_by_name("query")
+element=driver.find_element_by_name("query") #locate the Seach bar
 print(element.is_displayed())  #prints True/ False whether the search button is displayed or not
 print(element.is_enabled())    #prints True/ False whether the search button is enabled or not
 
